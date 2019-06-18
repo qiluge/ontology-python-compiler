888cbb51f230e9c4f827aa6195f9ddf409250300
qiluge <wangcheng@onchain.com>
2019-06-18 11:53:18 +0800

    add x-shard call api

--- ontology/interop/Ontology/Shard.py
+++ ontology/interop/Ontology/Shard.py
@@ -1,2 +1,25 @@
 def GetShardId():
     pass
+
+
+def NotifyRemoteShard(shardId, contract, fee, method, args):
+    """
+    :param shardId: target shard Id 
+    :param contract: target contract
+    :param fee: use gas
+    :param method: method name
+    :param args: byte array
+    :return: 
+    """
+    pass
+
+
+def InvokeRemoteShard(shardId, contract, method, args):
+    """
+    :param shardId: target shard Id 
+    :param contract: target contract
+    :param method: method name
+    :param args: byte array
+    :return: 
+    """
+    pass
